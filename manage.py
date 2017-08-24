243206e8a6d5d3ca3503bb83ba041b35caafa828
Ace <rossie@Roses-MacBook-Pro.local>
2017-08-24 17:22:21 +0300

    Add coveralls and travis

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 # manage.py
-
+import unittest
 import os
+
 from flask_script import Manager # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
+
 from app import db, create_app
 from app import models
 
@@ -11,6 +13,17 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """This automates tests by running tests"""
+    # gets test location and pattern
+    tests = unittest.TestLoader().discover('./tests', 'test*.py')
+    #runs the tests found
+    results = unittest.TextTestRunner(verbosity=2).run(tests)
+    if results.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
